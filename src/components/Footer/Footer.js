3e4599b1845dbbafc69a3e0dfc6e743e17617941
alkremn <alkremn@gmail.com>
2021-08-31 22:27:18 -0600

    feat: Implement footer section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,34 @@
-import React from 'react'
-import './Footer.css'
+import React from "react";
+import "./Footer.css";
 
 function Footer() {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <footer className='footer'>
+      <h2 className='footer__title'>
+        Учебный проект Яндекс.Практикум х BeatFilm.
+      </h2>
+      <div className='footer__buttom'>
+        <ul className='footer__links'>
+          <li className='footer__links-item'>
+            <a className='footer__link' href='https://practicum.yandex.ru/'>
+              Яндекс.Практикум
+            </a>
+          </li>
+          <li className='footer__links-item'>
+            <a className='footer__link' href='https://github.com/alkremn'>
+              Github
+            </a>
+          </li>
+          <li className='footer__links-item'>
+            <a className='footer__link' href='https://facebook.com'>
+              Facebook
+            </a>
+          </li>
+        </ul>
+        <p class='footer__copyright'>&copy;2020</p>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
