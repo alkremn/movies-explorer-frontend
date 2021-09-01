85d9194b7e42c14542c7924e27e28b5586d3dc94
alkremn <alkremn@gmail.com>
2021-08-31 23:57:38 -0600

    feat: implement register page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
+import Register from "../Register/Register";
 import Footer from "../Footer/Footer";
 
 function App() {
   return (
     <div className='page'>
-      <Header />
-      <Main />
-      <Footer />
+      {/* <Header /> */}
+      {/* <Main /> */}
+      <Register />
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ src/components/Register/Register.js
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Register.css";
+import logo from "../../images/logo.svg";
+
+function Register() {
+  return (
+    <div className='register'>
+      <div className='register__content'>
+        <img className='register__logo' src={logo} alt='logo' />
+        <h1 className='register__title'>Добро пожаловать!</h1>
+        <form className='register__form' action='' method='POST'>
+          <fieldset className='register__form-fieldset'>
+            <label className='register__input-label' htmlFor='name'>
+              Имя
+            </label>
+            <input className='register__input' type='text' id='name' />
+            <span className='register__input-error'>
+              Что-то пошло не так...
+            </span>
+          </fieldset>
+          <fieldset className='register__form-fieldset'>
+            <label className='register__input-label' htmlFor='email'>
+              E-mail
+            </label>
+            <input className='register__input' type='email' id='email' />
+            <span className='register__input-error'>
+              Что-то пошло не так...
+            </span>
+          </fieldset>
+          <fieldset className='register__form-fieldset'>
+            <label className='register__input-label' htmlFor='email'>
+              Пароль
+            </label>
+            <input className='register__input' type='password' id='password' />
+            <span className='register__input-error register__input-error_active'>
+              Что-то пошло не так...
+            </span>
+          </fieldset>
+          <button className='register__submit-button' type='submit'>
+            Зарегистрироваться
+          </button>
+          <p className='register__login-text'>
+            Уже зарегистрированы?
+            <span className='register__login-link'>Войти</span>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
