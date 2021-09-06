92b6ad69325668229f392889af7524af5d1bcb34
alkremn <alkremn@gmail.com>
2021-09-06 00:54:20 -0600

    feat: add valid links in portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,17 +7,26 @@ function Portfolio() {
       <h2 className='portfolio__title'>Портфолио</h2>
       <ul className='portfolio__links'>
         <li className='portfolio__links-item'>
-          <a className='portfolio__link' href='https://facebook.com'>
+          <a
+            className='portfolio__link'
+            href='https://alkremn.github.io/russian-travel/index.html'
+          >
             Статичный сайт<div className='portfolio__arrow-icon'></div>
           </a>
         </li>
         <li className='portfolio__links-item'>
-          <a className='portfolio__link' href='https://github.com/alkremn'>
+          <a
+            className='portfolio__link'
+            href='https://alkremn.github.io/russian-travel/index.html'
+          >
             Адаптивный сайт<div className='portfolio__arrow-icon'></div>
           </a>
         </li>
         <li className='portfolio__links-item'>
-          <a className='portfolio__link' href='https://github.com/alkremn'>
+          <a
+            className='portfolio__link'
+            href='https://akremn.students.nomoredomains.club'
+          >
             Одностраничное приложение
             <div className='portfolio__arrow-icon'></div>
           </a>
